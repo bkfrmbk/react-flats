6701041588e5799d10b1cfbea2b3ccce1307329b
Brad Korman <brad.korman@remarque-creative.com>
2020-03-11 23:33:05 -0400

    moved API key to gitignore

--- src/components/simple_map.jsx
+++ src/components/simple_map.jsx
@@ -17,7 +17,7 @@ class SimpleMap extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAMGJjDtGCF4PVFOM9FtRMkhvsJ7iaFArg' }}
+          bootstrapURLKeys={{ key: MAPP_API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
